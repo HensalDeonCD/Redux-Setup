c83f619acc31786e6941d517a4b031554fcbcf6d
HensalDeonCD <hensal@trusttech.in>
2024-06-06 11:13:19 +0530

    updated the way of using the redux thunk

--- src/redux/actions/clickAction.js
+++ src/redux/actions/clickAction.js
@@ -1,6 +1,20 @@
+// Normal way of using
 export const increment = () => ({
   type: "INCREMENT",
 });
 export const decrement = () => ({
   type: "DECREMENT",
 });
+
+
+// Action creator with Redux Thunk
+// export const increment = () => {
+//   return async (dispatch) => {
+//     dispatch({ type: "INCREMENT"});
+//   };
+// };
+// export const decrement = () => {
+//   return async (dispatch) => {
+//     dispatch({ type: "DECREMENT"});
+//   };
+// };
